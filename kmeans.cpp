a99be86b579fad18d96f879e79f9f6f76e0af3b2
Nava <nava@nava-VPCCW19FX.(none)>
2012-12-02 17:23:50 -0800

    kmeans works now! until it does not again

--- kmeans.cpp
+++ kmeans.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cv.h>
+#include <time.h>
+#include "kmeans.h"
+#include <stdlib.h>
 using namespace std;
 using namespace cv;
-
 int min(Mat *data, Mat *min) {
     int i = 0;
     double min_val0, min_val1, min_val2;
@@ -10,11 +12,11 @@ int min(Mat *data, Mat *min) {
     min_val0 = min_val1 = min_val2 = 10000;
 
     for(i = 0; i < data->rows; i++) {
-        if(min_val0 < data->at<double>( i, 0))
+        if(min_val0 > data->at<double>( i, 0))
             min_val0 = data->at<double>(i, 0);
-        if(min_val1 < data->at<double>( i, 1))
+        if(min_val1 > data->at<double>( i, 1))
             min_val1 = data->at<double>(i, 1);
-        if(min_val2 < data->at<double>( i, 2))
+        if(min_val2 > data->at<double>( i, 2))
             min_val2 = data->at<double>(i, 2);
     }
 
@@ -28,14 +30,14 @@ int min(Mat *data, Mat *min) {
 int max(Mat *data, Mat *max) {
     int i = 0;
     double max_val0, max_val1, max_val2; 
-    max_val0 = max_val1 = max_val1 = -10000;
-
+    max_val0 = max_val1 = max_val2 = -10000;
+    
     for(i = 0; i < data->rows; i++) {
-        if(max_val0 > data->at<double>( i, 0))
+        if(max_val0 < data->at<double>( i, 0))
             max_val0 = data->at<double>(i, 0);
-        if(max_val1 > data->at<double>( i, 1))
+        if(max_val1 < data->at<double>( i, 1))
             max_val1 = data->at<double>(i, 1);
-        if(max_val2 > data->at<double>( i, 2))
+        if(max_val2 < data->at<double>( i, 2))
             max_val2 = data->at<double>(i, 2);
     }
 
@@ -44,11 +46,19 @@ int max(Mat *data, Mat *max) {
     max->at<double>(0,2) = max_val2;
 
     return 0;
-} 
+}
  
+int fill_rand_matrix(Mat *centroid) {
+    int i, j;
+    for(i = 0; i < centroid->rows; i++) {
+        for(j = 0; j < centroid->cols; j++) {
+            centroid->at<double>(i,j) = (double)rand()/RAND_MAX;
+        }
+    }
+}
 
-int kmeans(double data[60][3], double k) {
-    int idx[60];
+
+int kmeans(double data[60][3], double k, int idx[60], double arr_centroid[8][3], double pCluster[8][1]) {
     Mat cvdata;
     Mat data_min, data_max, data_diff;
     Mat centroid(8, 3, CV_64FC1);
@@ -58,6 +68,7 @@ int kmeans(double data[60][3], double k) {
     double rnum = 0, pos_diff = 1.0;
     memset(idx, 0, sizeof(idx));
 
+    srand(time(NULL));
     cvdata = Mat(60, 3, CV_64FC1, data); 
     data_min = Mat(1, 3, CV_64FC1);
     data_max = Mat(1, 3, CV_64FC1);
@@ -65,36 +76,34 @@ int kmeans(double data[60][3], double k) {
 
     min(&cvdata, &data_min);
     max(&cvdata, &data_max);
-    RNG rng(0);
-    rng.fill(centroid, RNG::UNIFORM, 0, 1);
+    data_diff = data_max - data_min;
 
+    fill_rand_matrix(&centroid);
     int i;
+    for(i = 0; i < 8; i++) {
+        printf("%f %f %f\n",  centroid.at<double>(i,0),  centroid.at<double>(i,1), centroid.at<double>(i,2));
+    }
     for(i = 0; i < centroid.rows; i++) {
         centroid.row(i) = centroid.row(i).mul(data_diff);
-        centroid.row(i) = centroid.row(i) + data_diff;
+        centroid.row(i) = centroid.row(i) + data_min;
     }
-
+    for(i = 0; i < 8; i++) {
+        printf("%f %f %f\n",  centroid.at<double>(i,0),  centroid.at<double>(i,1), centroid.at<double>(i,2));
+    }
     while(pos_diff > 0.0) {
         int d,c;
-        double min1_diff, diff1_2c;
-        Mat min_diff(1, 3, CV_64FC1);
-        Mat mat_diff(3, 3, CV_64FC1);
         for(d = 0; d < cvdata.rows; d++) {
+            Mat min_diff = cvdata.row(d) - centroid.row(1);
+            Mat mat_diff = min_diff * min_diff.t();
+            double min_diff_t = mat_diff.at<double>(0,0);
             int curAssignment = 1;
-            min_diff = cvdata.row(d) - centroid.row(1);
-            mat_diff = min_diff * min_diff.t();
-            curAssignment = 1;
             
-            for(c=1; c < 8; c++) {
-                Mat diff2c(1, 3, CV_64FC1);
-                Mat mat_diff2c(3, 3, CV_64FC1);
-                diff2c = cvdata.row(d) - centroid.row(c);
-                mat_diff2c = diff2c * diff2c.t();
-                min1_diff = sum(sum(mat_diff))[0];
-                diff1_2c  = sum(sum(mat_diff2c))[0];
-                if(min1_diff >= diff1_2c) {
+            for(c=1; c < k; c++) {
+                Mat diff2c = cvdata.row(d) - centroid.row(c);
+                Mat mat_diff2c = diff2c * diff2c.t();
+                if(min_diff_t >= mat_diff2c.at<double>(0,0)) {
                     curAssignment = c;
-                    min1_diff = diff1_2c;
+                    min_diff_t = mat_diff2c.at<double>(0,0);
                 }
             }
             idx[d] = curAssignment;    
@@ -108,22 +117,27 @@ int kmeans(double data[60][3], double k) {
             centroid.row(idx[d]) += cvdata.row(d);
             pointsInCluster.at<double>(idx[d],1) = pointsInCluster.at<double>(idx[d],1) + 1;
         }
-        for(c = 0; c < cvdata.rows; c++) {
+        for(c = 0; c < 8; c++) {
             if(pointsInCluster.at<double>(c,1) != 0) {
                 centroid.row(c) = centroid.row(c) / pointsInCluster.at<double>(c,1);
             } else {
                 Mat randx(1, 3, CV_64FC1);
-                rng.fill(randx, RNG::UNIFORM, 0, 1);
+                fill_rand_matrix(&randx);
                 centroid.row(c) = randx.mul(data_diff) + data_min;
             }
         }
         Mat posdiffMat = centroid - oldPositions;
         pos_diff = sum(sum(posdiffMat.mul(posdiffMat)))[0];
     }
+    int j;
+    for(i = 0; i < 8; i++) {
+        for(j = 0; j < 3; j++) {
+            arr_centroid[i][j] = centroid.at<double>(i,j);         
+        }
+        pCluster[i][0] = pointsInCluster.at<double>(i,0);
+    }
+
     return 0;
 }
 
 
-int main(void) {
-    return 0;
-}
