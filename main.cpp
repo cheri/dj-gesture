0ad103971ca70ad313c461135b2348ee3ce8f592
Sarah H <smharmon+gravatar@colby.edu>
2012-12-07 20:01:07 -0800

    main.cpp cleanse pass 1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /* 
- * Main function...just calls functions to test for now
+ * Main function
  */
 
 #include <vector>
@@ -10,7 +10,6 @@
 #include "get_point_clusters.h"
 #include "prior_transition_matrix.h"
 #include "dhmm_numeric.h"
-
 #include <unistd.h> 
 #define GetCurrentDir getcwd
 
@@ -18,9 +17,8 @@ using namespace std;
 int main()
 {
 
-    /* Initialize XYZ "data" vector*/
-    double data[60][10][3];
-
+    /* Initialize XYZ "data" vectors */
+    double training[60][10][3], testing[60][10][3]; 
 
     /* Get current directory path */
     char cCurrentPath[FILENAME_MAX];
@@ -33,44 +31,52 @@ int main()
     filepath += "/data/train/";
     testpath += "/data/test/";
 
+    string train_gesture = "circle";
+    string test_gesture = "circle";
+
     /* Grab XYZ data */
-    get_xyz_data(filepath, "circle", data);
-    //	testing = get_xyz_data(filepath, "circle");
+    get_xyz_data(filepath, train_gesture, training);    // train
+    get_xyz_data(testpath, test_gesture, testing);      // test
 
-    /* Print data */
-#if 0
-    for (int e = 0; e < 3; e++)
-    {
-        // x, y, or z
-        cout << ("***Group ");
-        cout << e;
-        cout << (":***\n");
+    /****INITIALIZE****/
+    double gestureRecThreshold = 0; // set below
+    int N = 8, D = 3, M = 12, LR = 2;
+    double TrainXClustered[10][60], TestXClustered[10][60], centroids[8][3];
+    get_point_centroids(training, N, D, centroids);
+    get_point_clusters(training, centroids, D, TrainXClustered);
+    get_point_clusters(testing, centroids, D, TestXClustered);
 
-        for (int c = 0; c < 10; c++)
-        {
-            cout << ("Column ");
-            cout << c;
-            cout << (":\n");
-            for (int d = 0; d < 60; d++)
-            {
-
-                cout << data[c][d][e];
-                cout << (" ");
-            }
-            cout << ("\n\n");
-        }
-    }
-#endif
-    /* Call get_point_centroids */
-    // N = # of states
-    int N = 8, D = 3;
-    int M = 12, LR = 2;
-    double XClustered[10][60];
-    double centroids[8][3];
-    get_point_centroids(data,N, D, centroids);
-    get_point_clusters(data, centroids, D, XClustered);
+    /****TRAINING****/
+    // Set priors:
     double P[12][12];
     prior_transition_matrix(M, LR, P);
+
+    vector<vector<double> > ATrainBinned;
+    ATrainBinned.resize(10);
+    for (int i=0; i<10; i++)
+    {
+        ATrainBinned[i].resize(60);
+    }
+
+    vector<vector<double> > pP;
+    pP.resize(12);
+    for (int i=0; i<12; i++)
+    {
+        pP[i].resize(12);
+    }    
+
+    vector<vector<double> > bins;
+    bins.resize(8);
+    for (int i=0; i<8; i++)
+    {
+        bins[i][1] = (double)(i+1);
+    }
+
+    // Train the model:
+    int cyc = 50;
+    dhmm_numeric(ATrainBinned, pP, bins, M, cyc, .00001);
+
+    /****TESTING****/    
 #if 0
 
     double sumLik = 0;
@@ -102,30 +108,5 @@ int main()
         fprintf('Recognition success rate: %f percent\n',100*recs/length(ATestBinned));
 
 #endif
-
-    vector<vector<double> > ATrainBinned;
-
-    ATrainBinned.resize(10);
-    for (int i=0; i<10; i++)
-    {
-        ATrainBinned[i].resize(60);
-    }
-
-    vector<vector<double> > pP;
-    pP.resize(12);
-    for (int i=0; i<12; i++)
-    {
-        pP[i].resize(12);
-    }    
-
-    vector<vector<double> > bins;
-    bins.resize(8);
-    for (int i=0; i<8; i++)
-    {
-        bins[i][1] = (double)(i+1);
-    }
-
-    dhmm_numeric(ATrainBinned, pP, bins, M,50,.00001);
-
     return 0;
 } 
